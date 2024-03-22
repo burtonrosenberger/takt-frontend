169af1945b8b321d6c5c7bec6c90c561e44299f5
Marcus Teixeira <mteixeira.dev@gmail.com>
2024-03-22 20:16:18 -0300

    start new layout

--- app.vue
+++ app.vue
@@ -5,6 +5,17 @@
   </v-app>
 </template>
 <style>
+
+@font-face {
+    font-family: 'Futura';
+    src:  url('@/assets/font/unicode.futurab.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
+h1,h2,h3, p, span { 
+  color:#374151;
+}
 html { 
   overflow: auto;
+  font-family: 'Futura';
 }</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,33 @@
 <template>
-    <v-container fluid class="fill-height" :style="{ backgroundImage: `url('https://takt.soultech.solutions/assets/df075b4a-fc7d-425a-85d1-f523cecf8864')` }">
+    <v-container fluid class="fill-height" :style="{ backgroundColor: `#fcb34a` }">
         <v-row class="fill-height">
-            <v-col class=" d-flex flex-column fill-height" cols="12" sm="6">
-                <div style="height:90%;" class=" d-flex flex-column justify-space-around py-15 align-center">
-                    <v-carousel v-model="slide" hide-delimiters cycle :show-arrows="false" style="width:60%">
-                        <v-carousel-item v-for="fact in panel.facts">
-                            <v-card color="#00000085" class="white">
-                                <v-card-title>
-                                    Did you know...
-                                </v-card-title>
-                                <v-card-text>
-                                    {{ fact.text }}
-                                </v-card-text>
-                            </v-card>
-                        </v-carousel-item>
-                    </v-carousel>
-                    
-                    <v-card width="30%" color="#00000090" elevation="5" class="white" v-if="panel.about">
-                        <v-card-title>
-                            About
-                        </v-card-title>
-                        <v-card-text>
-                           {{ panel.about }}
-                        </v-card-text>
-                    </v-card>
-                </div>
-                <div class="text-left ">
-                    <p class="white">You are at</p>
-                    <h1 class="white">{{ panel.title }}</h1>
-                </div>
+            <v-col class=" d-flex flex-column fill-height" cols="12" sm="7">
+                <img :src="logo" class="" width="100" alt="Der Takt">
+                <h1>Revolutionizing Public Engagement Services:<br> Bringing Participation to the People, On the Go.</h1>
+
+
+                <v-row class="d-flex align-center">
+                    <v-col v-for="b in buttons">
+                        <div class="question">
+                            <span>Click here to</span>
+                            <p>{{b.title}}</p>
+                        </div>
+                        <v-img width="100%" max-height="350px" contain   :src="b.img">
+                        </v-img>
+                    </v-col>
+                </v-row>
+               
             </v-col>
-            <v-col class="fill-height" cols="12" sm="6">
-                <div style="height:90%;" class="w-100 align-center d-flex text-center">
-                    <p>
-                        <v-btn :to="btn.route" v-for="btn in buttons">{{ btn.title }}</v-btn>
-                    </p>
+            <v-col class="fill-height" cols="12" sm="5">
+                <div class="w-100 align-center d-flex text-center">
+                    <v-img :src="landMarks" height="90vh" class="d-flex align-center justify-center" cover>
+                        <v-img :src="marker" class="mx-auto" cover width="100px"></v-img>
+                        </v-img>
                 </div>
                 <div>
                     <p class=" d-flex justify-end align-center">
                         <v-btn>German</v-btn>
-                        <v-btn>Netherlands</v-btn>
-                        <v-btn>Ukraine</v-btn>
-                        <img :src="logo" class="ml-5" width="100" alt="Der Takt">
+                        <v-btn>English</v-btn>
                     </p>
                 </div>
             </v-col>
@@ -49,29 +35,39 @@
     </v-container>
 </template>
 <style scoped>
-.v-container { 
-    background-position: center;
-    background-size: cover;
+.question { 
+    background-color:#374151;
+    padding:0px 10px 10px;
 }
-.white { 
-    color:white
+.question span { 
+    color: white;
+    font-size:12px;
 }
-.h-90 {
-    height: 90%;
+.question p { 
+    background-color:white;
+    padding:5px;
 }
 </style>
 <script setup>
-import logo from '@/public/logo.png';
+import logo from '@/assets/logo.png';
+import marker from '@/assets/marker.png';
+
+import job_offers from '@/assets/job_offers.gif';
+import questions from '@/assets/questions.gif';
+import semminar from '@/assets/semminar.gif';
+import rollers from '@/assets/rollers.gif';
+
+import landMarks from '@/assets/A3_landmarks.png';
 import { onMounted, ref } from 'vue';
 
 const panel = ref({})
 const slide = ref(0)
 
 const buttons = ref([
-    { title: "Play a short game!", route: "/games" },
-    { title: "Learn how to participate!", route: "/how-to-participate" },
-    { title: "Check out Aachen`s Green Projects!", route: "/green-projects" },
-    { title: "Participate in surveys!", route: "/participate-survey" },
+    { title: "How to participate!", img: questions, route: "/how-to-participate" },
+    { title: "Play a game!", img: rollers, route: "/games" },
+    { title: "Know more about...", img:semminar, route: "/green-projects" },
+    { title: "Make your vote count!", img:job_offers, route: "/participate-survey" },
 ])
 
 const loadLocation = async () => { 
