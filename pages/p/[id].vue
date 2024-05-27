0f27a5c68c32e50a8d6ec0bc34818c3781ce6342
Marcus Teixeira <mteixeira.dev@gmail.com>
2024-05-27 18:40:27 -0300

    loading screen and start translate dynamic content

--- pages/p/[id].vue
+++ pages/p/[id].vue
@@ -15,25 +15,53 @@
         <v-main>
             <v-container class="pa-0" fluid>
                 <Game :games="panel.games" id="games"></Game>
-                <Home :projects="panel.projects" :desc="panel.description" :location="{x: panel.marker_x, y:panel.marker_y}" ref="homeRef"></Home>
+                <Home :projects="panel.projects" :desc="description" :location="{x: panel.marker_x, y:panel.marker_y}" ref="homeRef"></Home>
                 <Participate :participations="panel.participate" id="participate"></Participate>
                 <Survey :questions="panel.questions" id="survey"></Survey>
             </v-container>
         </v-main>
+        <v-row v-if="loading" class="loading">
+            <v-col cols="12" class="fill-height d-flex align-center justify-center">
+                <p>
+                    <v-img class="ct-animate-blink" :src="logo" cover max-width="350px" height="30px"  alt="Der Takt" />
+                    <br>loading....
+                </p>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 <style scoped>
+.loading { 
+    position:fixed;
+    top:0;
+    left:0;
+    min-width:101vw;
+    height:101vh;
+    background:#fcb34a;
+    color:#374151;
+    z-index:9999;
+}
+.ct-animate-blink {
+   animation: blink 1.5s infinite;
+   animation-fill-mode: both;
+}
 
+@keyframes blink {
+   0% { opacity: 0 }
+   50% { opacity: 1 }
+   100% { opacity: 0 }
+}
 </style>
 <script setup>
-const {  setLocale } = useI18n()
+const {  setLocale, locale } = useI18n()
 import logo from '@/assets/logo.png';
 import german from '@/assets/lang/deu.png';
 import english from '@/assets/lang/eng.png';
 import { onMounted, ref } from 'vue';
 
-const homeRef = ref( null)
-const panel = ref({title:"", projects:[], games:[], participations:[]})
+const loading = ref(true)
+const homeRef = ref(null)
+const panel = ref({title:"", projects:[], games:[], participations:[], translations: []})
 const route = useRoute()
 const menu = [
 { title:"Home", link:"#home" },
@@ -48,16 +76,23 @@ const scrollInto = (id) => {
     // if(id === `#home`) window.scrollY=0
 }
 
+const description = computed(() => { 
+    const lang = panel.value.translations?.find((el) => el.languages_code === locale.value)
+    return lang?.description
+})
+
 const loadLocation = async () => {
     let url = route.params.id ? route.params.id : "current-location"
 
     const { data } = await $fetch(
-        'https://armn.takt.city/items/locations?fields=*,projects.projects_id.*,participate.participate_id.*,questions.questions_id.*,questions.questions_id.answers.*,games.games_id.*,games.games_id.columns.left,games.games_id.columns.right,games.games_id.columns.id&filter[url][_eq]='+ url,
+        'https://armn.takt.city/items/locations?fields=*,translations.*,projects.projects_id.*,participate.participate_id.*,questions.questions_id.*,questions.questions_id.answers.*,games.games_id.*,games.games_id.columns.left,games.games_id.columns.right,games.games_id.columns.id&filter[url][_eq]='+ url,
         {
             headers: new Headers({'Authorization':  "Bearer j04rZ3-gVM-SyJlK-iAE1MH5HDbovh1u"})
         })
     if (data[0]) panel.value = data[0]
-    // console.log(panel.value)
+    
+    setTimeout(() => { loading.value = false} , 3000)
+
 
 }
 
